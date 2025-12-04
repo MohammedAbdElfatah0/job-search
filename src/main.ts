788f35a1980c4995f39fdd83c76e330867e0e471
Mohammed Mohammed Abd El-Fatah <hamoabdelfatah611@gmail.com>
2025-12-04 14:31:27 +0200

    add  cors for porject

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import  helmet from 'helmet';
+import helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.use(helmet());
   app.useGlobalPipes(new ValidationPipe({
     stopAtFirstError: true,
-    whitelist:true,
+    whitelist: true,
   }))
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
