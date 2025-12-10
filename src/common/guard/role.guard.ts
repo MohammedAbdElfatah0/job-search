c0ccead4cde7916909a8a5b9823ade81522a33b1
Mohammed Mohammed Abd El-Fatah <hamoabdelfatah611@gmail.com>
2025-12-10 20:14:30 +0200

    clean code of file role.guard

--- src/common/guard/role.guard.ts
+++ src/common/guard/role.guard.ts
@@ -19,8 +19,6 @@ export class RolesGuards implements CanActivate {
         const roles = this.reflector.getAllAndMerge(Roles, [context.getHandler(), context.getClass()])
         const Public = this.reflector.get(PUBLIC, context.getHandler());
         if (Public) return true;
-
-
         switch (context.getType<string>()) {
             case "http":
                 request = context.switchToHttp().getRequest();
@@ -33,7 +31,6 @@ export class RolesGuards implements CanActivate {
                 request = context.switchToWs().getClient();
                 break;
         }
-        console.log(request.user.role)
         if (!roles.includes(request.user.role)) throw new UnauthorizedException("you are Not Allow");
         return true;
     }
