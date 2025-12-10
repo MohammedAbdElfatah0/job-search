f7f0ce96733c55f58492d7fe48ba0453ad5330a2
Mohammed Mohammed Abd El-Fatah <hamoabdelfatah611@gmail.com>
2025-12-10 14:26:06 +0200

    done Feat: Admin Dashboard

--- src/module/admin/admin.reslove.ts
+++ src/module/admin/admin.reslove.ts
@@ -1,18 +1,21 @@
-import { Resolver, Query, Mutation, Args } from "@nestjs/graphql";
-import { AdminGQL } from "./admin.graphql";
-import { UserService } from "../user/user.service";
-import { CompanyService } from "../compeny/company.service";
-import { UserRepository } from "src/DB";
-import { USER_ROLE } from "src/common";
 import { ForbiddenException, NotFoundException } from "@nestjs/common";
+import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
+import { UserRepository } from "src/DB";
+import { CompanyRepository } from "src/DB/model/compeny/compeny.repository";
+import { USER_ROLE } from "src/common";
+import { CompanyService } from "../compeny/company.service";
 import { UserGQL } from "../user/user.resolve";
+import { UserService } from "../user/user.service";
+import { AdminGQL } from "./admin.graphql";
+import { CompanyGQL } from "../compeny/company.resolve";
 
 @Resolver()
 export class AdminReslove {
     constructor(
         private readonly userService: UserService,
         private readonly userRepository: UserRepository,
-        private readonly companyService: CompanyService
+        private readonly companyService: CompanyService,
+        private readonly companyRepository: CompanyRepository
     ) { }
     @Query(() => AdminGQL)
     async displayAllData() {
@@ -26,9 +29,9 @@ export class AdminReslove {
     }
 
     //admin make user ban and 
-    async banOrUnbanUser(id: string) {
+    private async banOrUnbanUser(id: string) {
         //check user exist
-        const userExist = await this.userRepository.getOne({ _id: id });
+        const userExist = await this.userRepository.getOne({ _id: id, deletedAt: { $exists: false } });
         if (!userExist) {
             throw new NotFoundException('User not found');
         }
@@ -39,9 +42,40 @@ export class AdminReslove {
         return userBan;
     }
     @Mutation(() => UserGQL)
-    async toBanOrUnBan(@Args('userId') userId: string) {
+    async BanOrUnBanUser(@Args('userId') userId: string) {
         const user = await this.banOrUnbanUser(userId);
-        return  user ;
+        return user;
+
+    }
+    private async banAndUnBan(id: string) {
+        const companyExist = await this.companyRepository.getOne({ _id: id, deletedAt: { $exists: false } })
+        if (!companyExist) {
+            throw new NotFoundException('Company not found');
+        }
+        const compantBan = await this.companyRepository.updateOne({ _id: id }, { $set: { bannedAt: companyExist.bannedAt ? null : new Date() } });
+        return compantBan;
+    }
+    @Mutation(() => CompanyGQL)
+    async BanOrUnBanCopmany(@Args('userId') userId: string) {
+        const user = await this.banAndUnBan(userId);
+        return user;
+
+    }
+
+    //approve company
+    private async approveCompany(id: string) {
+        const companyExist = await this.companyRepository.getOne({ _id: id, deletedAt: { $exists: false }, bannedAt: { $eq: null } })
+        if (!companyExist) {
+            throw new NotFoundException('Company not found');
+        }
+        const companyApprove = await this.companyRepository.updateOne({ _id: id }, { $set: { approvedByAdmin: true } });
+        return companyApprove;
+
+    }
+    @Mutation(() => CompanyGQL)
+    async ApproveCompany(@Args('userId') userId: string) {
+        const user = await this.approveCompany(userId);
+        return user;
 
     }
 
